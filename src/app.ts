d4ccfc6de9f8aef6d8bc5e3f3286727f93613c1a
roger <roogerbrito1@gmail.com>
2023-11-02 00:40:22 -0300

    adicionado cors no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,23 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import router from './routes/champions';
 import connection from './database/connection';
+import cors from 'cors';
 
 const app: Application = express();
+
+const allowedUrls = ['http://localhost:3000', 'http://localhost:5500'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedUrls.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    };
+  },
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 
 app.use(express.json());
 app.use('/api/', router);
